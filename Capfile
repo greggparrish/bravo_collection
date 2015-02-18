2466c8dd35397735491251862575d4346023ec13
Gregg Parrish <gregg.alb@gmail.com>
2015-02-18 01:11:13 +0100

    Added images

--- Capfile
+++ Capfile
@@ -2,8 +2,8 @@
 require 'capistrano/setup'
 
 # Includes default deployment tasks
-require 'capistrano/deploy'
 require 'capistrano/passenger'
+require 'bundler/capistrano'
 
 # Loads custom tasks from `lib/capistrano/tasks' if you have any defined.
 Dir.glob('lib/capistrano/tasks/*.cap').each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -4,11 +4,27 @@ lock '3.1.0'
 set :application, 'bravo_collection'
 set :repo_url, 'git@github.com:greggparrish/bravo_collection.git'
 set :deploy_to, '/home/ubuntu/public/web/bravo'
+  
+set :rails_env, "deploy"  # will this effect other built-in tasks? Maybe. 
+set :keep_releases, 5
 
 namespace :deploy do
-
-  after :publishing, :restart
-  set :rails_env, "deploy"  # will this effect other built-in tasks? Maybe. 
-  set :keep_releases, 5
-  after "deploy:restart", "deploy:cleanup" 
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
   end
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+  after :finishing, 'deploy:cleanup'
+
+end
