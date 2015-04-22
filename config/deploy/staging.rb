ace8fa6324cec0253a0fcd681a846248bd7818ab
hab278 <hab278@nyu.edu>
2015-04-22 16:03:59 -0400

    Make development git branch based on environment in deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,2 +1,9 @@
 set :rails_env, "staging"
-set(:branch, "master")
+
+def env_git_branch
+  ENV["GIT_BRANCH"].gsub(/remotes\//,"").gsub(/origin\//,"")
+rescue NoMethodError => msg
+  raise ArgumentError, "Environment variable 'GIT_BRANCH' was not set\n#{msg}"
+end
+
+set(:branch, env_git_branch)
