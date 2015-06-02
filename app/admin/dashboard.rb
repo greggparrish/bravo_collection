20d915fa80c4dc3cdb86eae11ec8485fc8ad427c
Gregg Parrish <gregg.alb@gmail.com>
2015-06-02 16:55:43 +0100

    remove dash sidebar

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,10 +2,6 @@ ActiveAdmin.register_page "Dashboard" do
 
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
-  sidebar :sidebar do
-    "Add sidebar?"
-  end
-  
   content :title => proc{ I18n.t("active_admin.dashboard") } do
     columns do
       column do
