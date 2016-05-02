13bb50643d93b20dcfa7138932ac780557312565
Eric Griffis <eric.griffis@nyu.edu>
2016-05-02 12:56:38 -0400

    Finding Aids - add donor list

---
+++ app/helpers/finding_aids_helper.rb
@@ -0,0 +1,16 @@
+module FindingAidsHelper
+  def donor_list
+    [
+      "Anonymous",
+      "Double R Foundation",
+      "JKW Foundation",
+      "Carol-Jeanette and Rickard Jorgensen",
+      "May and Samuel Rubin Foundation",
+      "Samuel Rubin Foundation",
+      "Center for the United States and the Cold War",
+      "Frederic Ewen Center",
+    ]
+  end
+
+  
+end
